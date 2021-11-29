261ea817907bdd705c0303e41e736c4547e551a7
tianzhuqingying <tianzhuqingying@gmail.com>
2021-11-29 17:44:01 +0000

    Create index html

---
+++ 001/js/001.js
@@ -0,0 +1,11 @@
+
+
+// setTimeout(function() {
+// }, 3000);
+  d3.select("body")
+    .select(".d3-wrapper")
+    .text("Hello D3")
+    .style("color", "red")
+    .style("font-size", "30px")
+    .attr('width', "20");
+
