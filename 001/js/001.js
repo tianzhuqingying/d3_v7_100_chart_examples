d27a46666d4a0e7ad152b4dac74a5b66a2411f38
tianzhuqingying <tianzhuqingying@gmail.com>
2021-12-02 16:25:16 +0000

    001 Update

--- 001/js/001.js
+++ 001/js/001.js
@@ -1,11 +1,11 @@
 
+// d3.select("body").
+// d3.selectAll(".d3-wrapper")
 
-// setTimeout(function() {
-// }, 3000);
-  d3.select("body")
-    .select(".d3-wrapper")
-    .text("Hello D3")
-    .style("color", "red")
-    .style("font-size", "30px")
-    .attr('width', "20");
+d3.selectAll("body .d3-wrapper")
+  .text("Hello D3")
+  .attr('class', "d3-set-attributes")
+  .style("color", "red")
+  .style("font-size", "30px");
+  .attr("style", "color: green; font-size: 30px;");
 
