bc79eda5832481cd641dd2de15d42c2b2424ecc7
tianzhuqingying <tianzhuqingying@gmail.com>
2021-12-03 23:02:54 +0000

    Update 001 js

--- 001/js/001.js
+++ 001/js/001.js
@@ -1,2 +1,6 @@
 // Version D3 7.1.1
-d3.select(".d3-wrapper")
+d3.selectAll("#d3-example .mt-3 .d3-wrapper")
+  .text("Hello world D3 class")
+  .attr('class', "d3-set-attributes")
+  .style("color", "#ffce56")
+  .style("font-size", "30px");
