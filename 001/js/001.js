c536fa12e68a0719efca6a995627f5a1ad55c6a5
tianzhuqingying <tianzhuqingying@gmail.com>
2021-12-03 03:19:58 +0000

    Update 001 index html

--- 001/js/001.js
+++ 001/js/001.js
@@ -1,11 +1,2 @@
-
-// d3.select("body").
-// d3.selectAll(".d3-wrapper")
-
-d3.selectAll("body .d3-wrapper")
-  .text("Hello D3")
-  .attr('class', "d3-set-attributes")
-  .style("color", "red")
-  .style("font-size", "30px");
-  .attr("style", "color: green; font-size: 30px;");
-
+// Version D3 7.1.1
+d3.select(".d3-wrapper")
